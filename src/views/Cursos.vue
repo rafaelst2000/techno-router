d8c2c413dba8b971bc949432b5a8aa0e7d4d5f8f
Rafael Trevisan <rafaelst2000@gmail.com>
2020-10-07 19:14:47 -0300

    techno started

---
+++ src/views/Cursos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading />
+    </div>
+    <transition>
+      <div v-if="api">
+        <h1>Cursos</h1>
+        <p>{{ api }}</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+
+
+<script>
+import fetchData from "@/mixins/fetchData.js"
+export default {
+  mixins: [fetchData],
+
+  created() {
+    this.fetchData("/cursos")
+  },
+}
+</script>
+
+<style>
+</style>
